e5666dfe3221300541ac88ecf850289926c34e4e
2Dream <alexeydream2@gmail.com>
2025-10-04 02:19:34 +0300

    C5 - added CQRS commands and queries abstractions. Adding logging and validation behaviors#1.

---
+++ TestApplication.UseCase/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplication.UseCase.Abstractions.Messaging;
+
+namespace TestApplication.UseCase.Abstractions.Behaviors
+{
+    public class LoggingBehavior
+    {
+        public class CommandHandler : ICommandHandler
+        {
+
+        }
+
+    }
+}
---
+++ TestApplication.UseCase/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication.UseCase.Abstractions.Messaging
+{
+    public interface ICommand : IBaseCommand;
+    public interface ICommand<TResponse> : IBaseCommand;
+    public interface IBaseCommand;
+}
