b67ec7cfd5638cefa8a1bc6afdb1cd3cd13dac70
2Dream <alexeydream2@gmail.com>
2025-10-04 03:03:26 +0300

    C5 - adding validation and logging behaviors#2.

---
+++ TestApplication.UseCase/Abstractions/Behaviors/ValidationBehaviorDecorator.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApplication.Domain.Results;
+using TestApplication.UseCase.Abstractions.Messaging;
+
+namespace TestApplication.UseCase.Abstractions.Behaviors
+{
+    public static class ValidationBehaviorDecorator
+    {
+        public class CommandHandler<TCommand>(
+            ICommandHandler<TCommand> innerHandler,
+            IValidator<TCommand> validator)
+            : ICommandHandler<TCommand>
+            where TCommand : ICommand
+        {
+            public async Task<Result> Handle(TCommand command, CancellationToken token = default)
+            {
+                var validationResult = await validator.ValidateAsync(command, token);
+                if (!validationResult.IsValid)
+                {
+                    string errors = string.Join("\n", validationResult.Errors.Select(e => e.ErrorMessage));
+                    return Result.Failure(errors);
+                }
+
+                var result = await innerHandler.Handle(command, token);
+                return result;
+            }
+        }
+        public class CommandHandler<TCommand, TResponse>(
+            ICommandHandler<TCommand, TResponse> innerHandler,
+            IValidator<TCommand> validator)
+            : ICommandHandler<TCommand, TResponse>
+            where TCommand : ICommand<TResponse>
+        {
+            public async Task<Result<TResponse>> Handle(TCommand command, CancellationToken token = default)
+            {
+                //if (validator != null)
+                //{
+                    var validationResult = await validator.ValidateAsync(command, token);
+                    if (!validationResult.IsValid)
+                    {
+                        string errors = string.Join("\n", validationResult.Errors.Select(e => e.ErrorMessage));
+                        return Result<TResponse>.Failure(errors);
+                    }
+                //}
+
+                var result = await innerHandler.Handle(command, token);
+                return result;
+            }
+        }
+        public class QueryHandler<TQuery, TResponse>(
+            IQueryHandler<TQuery, TResponse> innerHandler,
+            IValidator<TQuery> validator)
+            : IQueryHandler<TQuery, TResponse>
+            where TQuery : IQuery<TResponse>
+        {
+            public async Task<Result<TResponse>> Handle(TQuery query, CancellationToken token = default)
+            {
+                var validationResult = await validator.ValidateAsync(query, token);
+                if (!validationResult.IsValid)
+                {
+                    string errors = string.Join("\n", validationResult.Errors.Select(e => e.ErrorMessage));
+                    return Result<TResponse>.Failure(errors);
+                }
+
+                var result = await innerHandler.Handle(query, token);
+                return result;
+            }
+        }
+
+    }
+}
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql.NameTranslation;
+using System.Reflection;
 using TestApplication.Infrastructure.Databases.Write;
+using TestApplication.UseCase.Abstractions.Behaviors;
+using TestApplication.UseCase.Abstractions.Messaging;
 
 namespace TestApplication
 {
@@ -74,7 +78,31 @@ namespace TestApplication
 
             var builder = WebApplication.CreateBuilder(args);
 
+            
             builder.Services.AddScoped<AppDbContext>();
+            builder.Services.Scan(
+                scan => scan.FromAssembliesOf(typeof(AppDbContext))
+                    .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Repository")))
+                        .AsMatchingInterface()
+                        .WithScopedLifetime());
+            builder.Services.Scan(
+                scan => scan.FromAssembliesOf(typeof(ICommandHandler<>))
+                    .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime()
+                    .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime()
+                    .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime()
+                );
+            builder.Services.Decorate(typeof(ICommandHandler<>), typeof(ValidationBehaviorDecorator.CommandHandler<>));
+            builder.Services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationBehaviorDecorator.CommandHandler<,>));
+            builder.Services.Decorate(typeof(IQueryHandler<,>), typeof(ValidationBehaviorDecorator.QueryHandler<,>));
+            builder.Services.Decorate(typeof(ICommandHandler<>), typeof(LoggingBehaviorDecorator.CommandHandler<>));
+            builder.Services.Decorate(typeof(ICommandHandler<,>), typeof(LoggingBehaviorDecorator.CommandHandler<,>));
+            builder.Services.Decorate(typeof(IQueryHandler<,>), typeof(LoggingBehaviorDecorator.QueryHandler<,>));
 
 
             var app = builder.Build();
